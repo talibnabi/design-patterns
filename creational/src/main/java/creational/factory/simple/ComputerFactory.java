256afb67fc6b0b4fd9033af68e00c993b1b915f1
talibnabi <talibnabiyev@gmail.com>
2022-08-12 10:50:04 -0700

    commit

---
+++ creational/src/main/java/creational/factory/simple/ComputerFactory.java
@@ -0,0 +1,13 @@
+package creational.factory.simple;
+
+public final class ComputerFactory {
+    public static Computer getComputer(String type, String ram, String cpu, String hdd) {
+        if ("PC".equalsIgnoreCase(type)) {
+            return new PC(ram, cpu, hdd);
+        } else if ("SERVER".equalsIgnoreCase(type)) {
+            return new Server(ram, cpu, hdd);
+        } else {
+            return null;
+        }
+    }
+}
--- creational/src/main/java/creational/factory/simple/Server.java
+++ creational/src/main/java/creational/factory/simple/Server.java
@@ -1,4 +1,28 @@
 package creational.factory.simple;
 
-public class Server {
+public class Server extends Computer {
+    private final String ram;
+    private final String cpu;
+    private final String hdd;
+
+    public Server(String ram, String cpu, String hdd) {
+        this.ram = ram;
+        this.cpu = cpu;
+        this.hdd = hdd;
+    }
+
+    @Override
+    public String getRAM() {
+        return ram;
+    }
+
+    @Override
+    public String getCPU() {
+        return cpu;
+    }
+
+    @Override
+    public String getHDD() {
+        return hdd;
+    }
 }
