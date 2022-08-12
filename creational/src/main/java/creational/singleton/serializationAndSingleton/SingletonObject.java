0edcafc3cc909d800a232e31bacf593e1f9f1203
talibnabi <talibnabiyev@gmail.com>
2022-08-12 05:54:31 -0700

    commit

--- creational/src/main/java/creational/singleton/serializationAndSingleton/SingletonObject.java
+++ creational/src/main/java/creational/singleton/serializationAndSingleton/SingletonObject.java
@@ -5,4 +5,7 @@ import java.io.Serializable;
 public final class SingletonObject implements Serializable {
     private static final long serialVersionUID = -7604766932017737115L;
 
+    private SingletonObject() {
+        
+    }
 }
