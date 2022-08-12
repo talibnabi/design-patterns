4ccf5d4c6a25fff934d871662698d1401aa41cda
talibnabi <talibnabiyev@gmail.com>
2022-08-12 03:32:58 -0700

    commit

---
+++ creational/src/main/java/creational/singleton/billPughSingleton/SingletonObject.java
@@ -0,0 +1,10 @@
+package creational.singleton.billPughSingleton;
+
+public final class SingletonObject {
+    private static SingletonObject singletonObject;
+
+    private SingletonObject() {
+
+    }
+   
+}
