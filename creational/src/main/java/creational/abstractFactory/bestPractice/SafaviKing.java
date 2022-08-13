a2f8f82fcbd4a244afc362bf89c91c063b2408c9
talibnabi <talibnabiyev@gmail.com>
2022-08-13 04:06:13 -0700

    commit

--- creational/src/main/java/creational/abstractFactory/bestPractice/SafaviKing.java
+++ creational/src/main/java/creational/abstractFactory/bestPractice/SafaviKing.java
@@ -1,13 +1,21 @@
 package creational.abstractFactory.bestPractice;
 
-public class SafaviKing extends King{
+public class SafaviKing extends King {
+    private final String kingName;
+    private final Integer kingAge;
+
+    public SafaviKing(String kingName, Integer kingAge) {
+        this.kingName = kingName;
+        this.kingAge = kingAge;
+    }
+
     @Override
     public String getKingName() {
-        return null;
+        return this.kingName;
     }
 
     @Override
     public Integer getKingAge() {
-        return null;
+        return this.kingAge;
     }
 }
