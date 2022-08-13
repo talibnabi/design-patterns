65a163dc23fcab488ec8e970a98e4cdd417d94d3
talibnabi <talibnabiyev@gmail.com>
2022-08-13 02:34:23 -0700

    commit

--- creational/src/main/java/creational/abstractFactory/simpleFirst/App.java
+++ creational/src/main/java/creational/abstractFactory/simpleFirst/App.java
@@ -1,5 +1,8 @@
 package creational.abstractFactory.simpleFirst;
 
 public class App {
-    
+    public static void main(String[] args) {
+        Computer computer = ComputerFactory.getComputer(new ServerFactory("8", "1tb", "i7"));
+        System.out.println(computer.toString());
+    }
 }
