13fc12310becae7eca8335513d18426afa2b2de0
talibnabi <talibnabiyev@gmail.com>
2022-08-13 04:03:34 -0700

    commit

--- creational/src/main/java/creational/abstractFactory/bestPractice/SafaviCastle.java
+++ creational/src/main/java/creational/abstractFactory/bestPractice/SafaviCastle.java
@@ -1,18 +1,28 @@
 package creational.abstractFactory.bestPractice;
 
-public class SafaviCastle extends Castle{
+public class SafaviCastle extends Castle {
+    private final String castleName;
+    private final String castleLocation;
+    private final Integer castleAge;
+
+    public SafaviCastle(String castleName, String castleLocation, Integer castleAge) {
+        this.castleName = castleName;
+        this.castleLocation = castleLocation;
+        this.castleAge = castleAge;
+    }
+
     @Override
     public String getCastleName() {
-        return null;
+        return this.castleName;
     }
 
     @Override
     public String getCastleLocation() {
-        return null;
+        return this.castleLocation;
     }
 
     @Override
     public Integer getCastleAge() {
-        return null;
+        return this.castleAge;
     }
 }
