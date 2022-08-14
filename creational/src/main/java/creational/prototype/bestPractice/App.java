d556c44f9ce6246a1db426a69de1d0d4b9fc30f1
talibnabi <talibnabiyev@gmail.com>
2022-08-14 05:59:57 -0700

    commit

--- creational/src/main/java/creational/prototype/bestPractice/App.java
+++ creational/src/main/java/creational/prototype/bestPractice/App.java
@@ -2,6 +2,12 @@ package creational.prototype.bestPractice;
 
 public class App {
     public static void main(String[] args) {
-
+        HeroFactoryImpl heroFactory = new HeroFactoryImpl(new ElfBeast("x"), new ElfMage("y"), new ElfWarlord("z"));
+        Beast beast = heroFactory.createBeast();
+        Mage mage = heroFactory.createMage();
+        Warlord warlord = heroFactory.createWarlord();
+        System.out.println(beast);
+        System.out.println(warlord);
+        System.out.println(mage);
     }
 }
