3a088b0695fce302ae2d8f8f35d16639de1e0672
talibnabi <talibnabiyev@gmail.com>
2022-08-14 05:15:46 -0700

    commit

--- creational/src/main/java/creational/prototype/bestPractice/Beast.java
+++ creational/src/main/java/creational/prototype/bestPractice/Beast.java
@@ -1,12 +1,11 @@
 package creational.prototype.bestPractice;
 
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 @EqualsAndHashCode
+@NoArgsConstructor
 public abstract class Beast implements Prototype {
-
-    public Beast() {
-    }
 
     public Beast(Beast beast) {
 
