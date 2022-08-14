de1171acb5cb3f9e169d663ef3b7da787880b675
talibnabi <talibnabiyev@gmail.com>
2022-08-14 06:13:08 -0700

    commit

--- creational/src/main/java/creational/prototype/bestPractice/App.java
+++ creational/src/main/java/creational/prototype/bestPractice/App.java
@@ -9,5 +9,15 @@ public class App {
         System.out.println(beast);
         System.out.println(warlord);
         System.out.println(mage);
+
+
+        HeroFactoryImpl heroFactory2 = new HeroFactoryImpl(new ElfBeast("x"), new ElfMage("y"), new ElfWarlord("z"));
+        Beast beast2 = heroFactory2.createBeast();
+        Mage mage2 = heroFactory2.createMage();
+        Warlord warlord2 = heroFactory2.createWarlord();
+        System.out.println(beast2);
+        System.out.println(warlord2);
+        System.out.println(mage2);
+        System.out.println(beast.hashCode() == beast2.hashCode());
     }
 }
